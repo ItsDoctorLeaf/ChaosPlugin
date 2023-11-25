a92b495b1f05677da1ca85e018723d5f41c7d794
ItsDoctorLeaf <go.pennock@gmail.com>
2023-11-25 17:38:01 +0000

    Commit

---
+++ src/main/java/me/drl/chaosplugin/ChaosPlugin.java
@@ -0,0 +1,17 @@
+package me.drl.chaosplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChaosPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
